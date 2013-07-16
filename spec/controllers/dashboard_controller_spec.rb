62c963adc23dfcec770e3337c4acb2ce13bad4f9
Eric Sessoms <eric@nubgames.com>
2013-07-16 19:20:20 -0400

    using named routes in the dashboard

---
+++ spec/controllers/dashboard_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe DashboardController do
+  describe 'guest access' do
+    describe 'GET #show' do
+      it 'requires login' do
+        get :show
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
+
+  describe 'author access' do
+    before :each do
+      user = create :author
+      session[:user_id] = user.id
+    end
+
+    describe 'GET #show' do
+      it 'renders the show view' do
+        get :show
+        expect(response).to render_template :show
+      end
+    end
+  end
+end
